26c2c3103c51898a34a4e5065453b8e974401246
kimisak <isak7263@gmail.com>
2021-07-10 11:05:57 +0900

    WebConfig 한글 필터 추가

--- src/main/java/com/project/config/WebConfig.java
+++ src/main/java/com/project/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.project.config;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletRegistration;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebConfig
@@ -26,5 +28,16 @@ extends AbstractAnnotationConfigDispatcherServletInitializer{
 	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
 		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
 	}
+	
+	// 웹에서 등록될 때 한글 utf-8 허용 필터
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		//return super.getServletFilters();
+		return new Filter[] { characterEncodingFilter };
+	}
+	
 
 }
