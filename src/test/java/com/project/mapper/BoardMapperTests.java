f6e3aaa93842659d41d731118efed546967841fc
kimisak <isak7263@gmail.com>
2021-07-14 23:28:10 +0900

    페이지 값에 의한 페이징 처리 및 테스트 코드 작성

--- src/test/java/com/project/mapper/BoardMapperTests.java
+++ src/test/java/com/project/mapper/BoardMapperTests.java
@@ -1,5 +1,7 @@
 package com.project.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.project.config.RootConfig;
 import com.project.domain.BoardVO;
+import com.project.domain.Criteria;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -20,11 +23,12 @@ public class BoardMapperTests {
 	
 	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
-	
+	/*
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board->log.info(board));
 	}
+	*/
 	/*
 	@Test
 	public void testInert() {
@@ -50,18 +54,20 @@ public class BoardMapperTests {
 		log.info(board);
 	}
 	*/
-	
+	/*
 	@Test
 	public void testRead() {
 		BoardVO board = mapper.read(4L);
 		log.info(board); // 존재하는 게시물 번호로 테스트
 	}
+	*/
 	/*
 	@Test
 	public void testDelete() {
 		log.info("DELETE COUNT : "+mapper.delete(4L));
 	}
 	*/
+	/*
 	@Test
 	public void testUpdate() {
 		BoardVO board = new BoardVO();
@@ -71,5 +77,12 @@ public class BoardMapperTests {
 		board.setWriter("수정user");
 		log.info("UPDATE COUNT : "+mapper.update(board));
 	}
-	
+	*/
+	@Test
+	public void testPaging() {
+		Criteria cri = new Criteria();
+		cri.setPageNum(2);
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		list.forEach(board -> log.info(board));
+	}
 }
