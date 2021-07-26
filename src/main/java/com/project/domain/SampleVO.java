953463913940293c39d13d19d8d2fe0b5c2a4adb
kimisak <isak7263@gmail.com>
2021-07-26 23:20:27 +0900

    SampleContoller & VO 생성

---
+++ src/main/java/com/project/domain/SampleVO.java
@@ -0,0 +1,14 @@
+package com.project.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SampleVO {
+	private Integer no;
+	private String firstName;
+	private String lastName;
+}
