a975e85c5a56682b90b96529621c10e11e40dc0b
kimisak <isak7263@gmail.com>
2021-07-28 00:16:46 +0900

    @RestController의 getList 메서드, getMap 메서드 추가

--- src/main/java/com/project/controller/SampleController.java
+++ src/main/java/com/project/controller/SampleController.java
@@ -1,5 +1,11 @@
 package com.project.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +31,22 @@ public class SampleController {
 						MediaType.APPLICATION_XML_VALUE})
 	public SampleVO getSample() {
 		return new SampleVO(112, "길동", "홍");
+	}
+	@GetMapping(value="/getSample2")
+	public SampleVO getSample2() {
+		return new SampleVO(112, "길동", "고");
+	}
+	
+	@GetMapping(value="/getList")
+	public List<SampleVO> getList() {
+		return IntStream.range(1, 10).mapToObj(i -> new SampleVO(i, i + "First", i + " Last")).
+				collect(Collectors.toList());
+	}
+	
+	@GetMapping(value="/getMap")
+	public Map<String, SampleVO> getMap() {
+		Map<String, SampleVO> map = new HashMap<>();
+		map.put("First", new SampleVO(111, "바", "자"));
+		return map; 
 	}
 }
