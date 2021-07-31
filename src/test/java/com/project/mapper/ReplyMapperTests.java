e9b1e254dafe6692cded8c493488abfaf039b1cc
kimisak <isak7263@gmail.com>
2021-08-01 04:44:15 +0900

    스프링 댓글 처리를 위한 영속 속성 구현

--- src/test/java/com/project/mapper/ReplyMapperTests.java
+++ src/test/java/com/project/mapper/ReplyMapperTests.java
@@ -1,5 +1,7 @@
 package com.project.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.project.config.RootConfig;
+import com.project.domain.Criteria;
 import com.project.domain.ReplyVO;
 
 import lombok.Setter;
@@ -20,18 +23,19 @@ public class ReplyMapperTests {
 	
 	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
-	
+	/*
 	@Test
 	public void testMapper() {
 		log.info(mapper);
 		mapper.getListAll().forEach(reply->log.info(reply));
 	}
-	
+	*/
+	/*
 	// 등록
-	
 	@Test
 	public void testInsert() {
 		ReplyVO vo = new ReplyVO();
+		
 		for (int i = 0; i <= 5; i++) {
 			vo.setSeq_bno(748);
 			vo.setReply("댓글 테스트 : " + i);
@@ -39,26 +43,42 @@ public class ReplyMapperTests {
 			mapper.insert(vo);
 		}
 	}
+	*/
 	
-	/*
 	// 조회
+	/*
 	@Test
 	public void testRead() {
-		
-	}
-	// 수정
-	@Test
-	public void testUpdate() {
-		
-	}
-	// 삭제
-	@Test
-	public void testDelete() { 
-		
-	}
-	// 댓글 목록
-	public void testGetList() { 
-		
+		log.info(mapper.read(10L));
 	}
 	*/
+	
+	// 삭제
+	/*
+	@Test
+	public void testDelete() { 
+		mapper.delete(10L);
+	}
+	*/
+	
+	// 수정
+	/*
+	@Test
+	public void testUpdate() {
+		ReplyVO vo = new ReplyVO();
+		vo.setSeq_rno(11);
+		vo.setReply("수정 테스트");
+		int count = mapper.update(vo);
+		log.info("count : "+ count);
+	}
+	*/
+	
+	// 댓글 목록
+	@Test
+	public void testGetList() { 
+		Criteria cri = new Criteria();
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, 777L);
+		replies.forEach(reply -> log.info(reply));
+	}
+	
 }
