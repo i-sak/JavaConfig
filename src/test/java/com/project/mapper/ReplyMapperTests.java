0768ff88bebebd077129b1441e6d2f84a5929b68
kimisak <isak7263@gmail.com>
2021-07-31 21:57:16 +0900

    ReplyMapper interface / Mapper xml / Test

--- src/test/java/com/project/mapper/ReplyMapperTests.java
+++ src/test/java/com/project/mapper/ReplyMapperTests.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.project.config.RootConfig;
+import com.project.domain.ReplyVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -26,4 +27,38 @@ public class ReplyMapperTests {
 		mapper.getListAll().forEach(reply->log.info(reply));
 	}
 	
+	// 등록
+	
+	@Test
+	public void testInsert() {
+		ReplyVO vo = new ReplyVO();
+		for (int i = 0; i <= 5; i++) {
+			vo.setSeq_bno(748);
+			vo.setReply("댓글 테스트 : " + i);
+			vo.setReplyer("Replyer " + i);
+			mapper.insert(vo);
+		}
+	}
+	
+	/*
+	// 조회
+	@Test
+	public void testRead() {
+		
+	}
+	// 수정
+	@Test
+	public void testUpdate() {
+		
+	}
+	// 삭제
+	@Test
+	public void testDelete() { 
+		
+	}
+	// 댓글 목록
+	public void testGetList() { 
+		
+	}
+	*/
 }
